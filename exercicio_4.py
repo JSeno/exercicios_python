bf0a331d0baf6dd0ee791066ece84d818413bfc2
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-04-30 14:20:09 -0300

    alterando algumas condições

--- exercicio_4.py
+++ exercicio_4.py
@@ -15,18 +15,44 @@ print('Vamos começar!')
 print('Se você quiser jogar digite Sim, caso contrário digite Não.')
 aceitar_jogar = input('Digite sua escolha: ')
 
-if aceitar_jogar == 'sim' or aceitar_jogar == 'Sim' or aceitar_jogar == 's' or aceitar_jogar == 'S':
-    print('OK! Vamos começar!')
+def jogar():
     print('Escolha um número de 1 à 50.')
     numero_chutado = int(input('Digite seu número: '))
     numero_aleatorio = random.randint(1, 2)
+    if  numero_chutado == numero_aleatorio:
+        print('+----------------------------------+')
+        print('|\(*.*)/   Você acertou!    \(*.*)/|')
+        print('+----------------------------------+')
 
-    if numero_chutado == numero_aleatorio:
-        print('Parabéns! Você acertou!')
+    elif numero_chutado != numero_aleatorio:
+        print('+---------------------------------------------------------------+')
+        print(f'Que pena! Você errou o número era {numero_aleatorio} e você chutou {numero_chutado}')
+        print('+---------------------------------------------------------------+')
+        print('\nGostaria de tentar novamente? Digite Sim ou Não.')
+        aceitar_jogar = input('Digite sua escolha: ')
+        if aceitar_jogar == 'Sim' or aceitar_jogar == 'sim' or aceitar_jogar == 'S' or aceitar_jogar == 's':
+            jogar()
+        elif aceitar_jogar == 'Não' or aceitar_jogar == 'não' or aceitar_jogar == 'N' or aceitar_jogar == 'n':
+            print('Obrigado por jogar!')
+        else:
+            print('Opção inválida!')
 
+
+while aceitar_jogar == 'Sim' or aceitar_jogar == 'sim' or aceitar_jogar == 'S' or aceitar_jogar == 's':
+    if aceitar_jogar == 'sim' or aceitar_jogar == 'Sim' or aceitar_jogar == 's' or aceitar_jogar == 'S':
+        print('OK! Vamos começar!')
+        jogar()
+        print('Você quer jogar novamente? Digite. S/N')
+        aceitar_jogar = input('Digite sua escolha: ')
+        if aceitar_jogar == 'sim' or aceitar_jogar == 'Sim' or aceitar_jogar == 's' or aceitar_jogar == 'S':
+            jogar()
+        else:
+            print('Tchau!')
+
+    elif aceitar_jogar == 'não' or aceitar_jogar == 'Não' or aceitar_jogar == 'n' or aceitar_jogar == 'N':
+        print('Tudo bem, até mais!')
+        break
     else:
-        print('Que pena! Você errou!')
-        print('O número era {} e você digitou {}.'.format(numero_aleatorio, numero_chutado))
+        print('Opção inválida!')
+        break
 
-else:
-    print('Tudo bem! Até a próxima!')
